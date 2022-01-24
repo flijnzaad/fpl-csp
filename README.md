99b42280b83f49e9e2f009052be02975424676ab
jasperdeboomstam <jasperdeboomstam@gmail.com>
2022-01-24 14:45:00 +0100

    fixed constraint type

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Functional programming & constraint programming
 Project for the course "Functional Programming for Logicians" about constraint programming
-test hoi
--- ac-3.hs
+++ ac-3.hs
@@ -3,7 +3,8 @@ import Data.List -- we will probably need this at some point
 type Variable   = Int
 type Value      = Int
 type Domain     = (Variable, [Value])
-type Constraint = ( (Variable, Variable), (Value, Value) )
+type Constraint = ( (Variable, Variable), [(Value, Value)] )
 data Problem    = CSP { vars :: [Variable]
                       , doms :: [Domain]
                       , cons :: [Constraint] }
+
